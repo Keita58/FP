fb717febde433b9b691a7e434a19a5e279c39b88
Marc Sánchez <marc-s-26@hotmail.com>
2024-04-27 22:34:32 +0200

    Fet exercici de Node.js 1

---
+++ 1r/Programacio_web/M7/UF1/Angular/Exercicis/Node/src/app/server.ts
@@ -0,0 +1,178 @@
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const client = require("mongodb").MongoClient;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+//Creem el port
+app.listen(3000, function () {
+    console.log("Funciona! :D (Port 3000)");
+})
+module.exports = app;
+
+const url = "mongodb+srv://keita58:hFqS7KeKEkz9u20r@basededades.9tcii4i.mongodb.net/";
+const dbName = "albums";
+app.get('/', function(req, res) {
+    return res.send({error: false, message: 'holiis'});
+});
+
+app.get('/songs', async(req, res) => {
+    let client2;
+    try {
+        client2 = await client.connect(url);
+        const db = client2.db(dbName);
+        const songs = await db.collection("songs").find().toArray();
+        res.json(songs);
+    }
+    catch (err) {
+        console.log("Error amn l'operació CRUD de l'API", err);
+    }
+    finally {
+        if(client2) {
+            client2.close();
+        }
+    }
+});
+
+app.get('/songs/:reproduccions/:recaptacio', async(req, res) => {
+    let client2;
+    let reproduccions = Number(req.params.reproduccions);
+    let recaptacio = Number(req.params.recaptacio);
+    try {
+        client2 = await client.connect(url);
+        const db = client2.db(dbName);
+        const songs = await db.collection("songs").find({'Reproduccions': {$gte: reproduccions}, 'Recaptacio': {$gte: recaptacio}}).toArray();
+        res.json(songs);
+    }
+    catch (err) {
+        console.log("Error amn l'operació CRUD de l'API", err);
+    }
+    finally {
+        if(client2) {
+            client2.close();
+        }
+    }
+});
+
+app.get('/songs/insert', async(req, res) => {
+    let client2;
+    let nom = req.body.nom;
+    let reproduccions = Number(req.body.reproduccions);
+    let recaptacio = Number(req.body.recaptacio);
+    try {
+        client2 = await client.connect(url);
+        const db = client2.db(dbName);
+        let id = (await db.collection('songs').find().toArray()).length + 1;
+        const song = await db.collection('songs').insertOne({'id': id, 'Nom': nom, 'Reproduccions': reproduccions, 'Recaptacio': recaptacio});
+        res.json("Afegida!");
+    }
+    catch (err) {
+        console.log("Error amn l'operació CRUD de l'API", err);
+    }
+    finally {
+        if(client2) {
+            client2.close();
+        }
+    }
+});
+
+//-------------------------------------------------------------(Exercicis amb usuaris_app)
+
+app.get('/users', async(req, res) => {
+   let client2;
+   try{
+       client2 = await client.connect(url);
+       const db = client2.db(dbName);
+       const users = await db.collection("usuaris_app").find().toArray();
+       res.json(users);
+   }
+   catch(err){
+       console.log("Error amn l'operació CRUD de l'API", err);
+   }
+   finally {
+       if(client2) {
+           client2.close();
+       }
+   }
+});
+
+app.get('/users/:nom/:password', async(req, res) => {
+    let client2;
+    let nom = String(req.params.nom);
+    let password = String(req.params.password);
+    try {
+        client2 = await client.connect(url);
+        const db = client2.db(dbName);
+        const users = await db.collection("usuaris_app").find({'nom': nom, 'password': password}).toArray();
+        res.json(users.length);
+    }
+    catch (err) {
+        console.log("Error amn l'operació CRUD de l'API", err);
+    }
+    finally {
+        if(client2) {
+            client2.close();
+        }
+    }
+});
+
+app.get('/users/insert', async(req, res) => {
+    let client2;
+    let nom = req.body.nom;
+    let password = String(req.body.password);
+    try {
+        client2 = await client.connect(url);
+        const db = client2.db(dbName);
+        let id = (await db.collection('usuaris_app').find().toArray()).length + 1;
+        const user = await db.collection('usuaris_app').insertOne({'id': id, 'nom': nom, 'password': password});
+        res.json("Afegida!");
+    }
+    catch (err) {
+        console.log("Error amn l'operació CRUD de l'API", err);
+    }
+    finally {
+        if(client2) {
+            client2.close();
+        }
+    }
+});
+
+app.get('/users/update', async(req, res) => {
+    let client2;
+    let id = req.body.id;
+    let nom = req.body.nom;
+    let password = req.body.password;
+    try {
+        client2 = await client.connect(url);
+        const db = client2.db(dbName);
+        if(id !== undefined) {
+            if (nom === undefined && password !== undefined) {
+                let user = await db.collection('usuaris_app').updateOne({'id': id}, {$set: {'password': password}});
+                console.log("Password canviada");
+            } else if (nom !== undefined && password === undefined) {
+                let user = await db.collection('usuaris_app').updateOne({'id': id}, {$set: {'nom': nom}});
+                console.log("Correu canviat");
+            } else {
+                let user = await db.collection('usuaris_app').updateOne({'id': id}, {$set: {'nom': nom, 'password': password}});
+                console.log("Correu i password canviats");
+            }
+            res.json("Actualitzada!");
+        }
+        else
+            res.json("Et falta l'id!");
+    }
+    catch (err) {
+        console.log("Error amn l'operació CRUD de l'API", err);
+    }
+    finally {
+        if(client2) {
+            client2.close();
+        }
+    }
+});
