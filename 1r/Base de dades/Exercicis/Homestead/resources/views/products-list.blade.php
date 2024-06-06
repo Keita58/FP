335899eb91155d81bf293aa46085e661238b9b1e
Marc Sánchez <marc-s-26@hotmail.com>
2024-06-06 20:14:07 +0200

    Laravel a Debian

--- 1r/Base de dades/Exercicis/Homestead/resources/views/products-list.blade.php
+++ 1r/Base de dades/Exercicis/Homestead/resources/views/products-list.blade.php
@@ -1,24 +1,26 @@
-<link rel="stylesheet" type="text/css" href="{{ url('/resources/css/menu.css') }}" />
-<nav class="flex flex-1 justify-begin mr-1 background">
-    <a href="{{ route('inici') }}" class="lletraMenu primer">
-        Home
-    </a>
-    <a href="{{ route('clients.list') }}" class="lletraMenu">
-        Clients
-    </a>
-</nav>
-<div>
-    @foreach ($products as $product)
-        <div>
-            <div>{{ $product->name }}, {{ $product->price }}, {{ $product->description }}, {{ $product->quantity }}</div>
-            <button onclick="location.href='{{ url('/product/'.$product->id.'/warn') }}'">Delete</button>
-            <!-- Aquest botó ens porta directament al link d'eliminació del producte -->
-        </div>
-    @endforeach
-    <br>
+<x-app-layout>
+    <nav class="flex flex-1 justify-begin mr-1 background">
+        <a href="{{ route('inici') }}" class="lletraMenu primer">
+            Home
+        </a>
+        <a href="{{ route('clients.list') }}" class="lletraMenu">
+            Clients
+        </a>
+    </nav>
     <div>
-        <label>Click here to insert a new product</label>
+        @foreach ($products as $product)
+            <div>
+                <div>{{ $product->name }}, {{ $product->price }}, {{ $product->description }}, {{ $product->quantity }}</div>
+                <button onclick="location.href='{{ url('/product/'.$product->id.'/warn') }}'">Delete</button>
+                <!-- Aquest botó ens porta directament al link d'eliminació del producte -->
+            </div>
+        @endforeach
         <br>
-        <button onclick="location.href='{{ url('/product/insert') }}'">Insert</button>
+        <div>
+            <label>Click here to insert a new product</label>
+            <br>
+            <button onclick="location.href='{{ url('/product/insert') }}'">Insert</button>
+        </div>
     </div>
-</div>
+</x-app-layout>
+
