6590aefa7d3ce9d8e4f6655c2252ebd5ab67da13
Marc Sánchez <marc-s-26@hotmail.com>
2024-06-07 19:57:50 +0200

    Modificant html productes-llista

--- 1r/Base de dades/Exercicis/Homestead/resources/views/products-list.blade.php
+++ 1r/Base de dades/Exercicis/Homestead/resources/views/products-list.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <nav class="flex flex-1 justify-begin mr-1 background">
+    <nav class="flex bg-green-900 text-white justify-around">
         <a href="{{ route('inici') }}" class="lletraMenu primer">
             Home
         </a>
@@ -7,20 +7,32 @@
             Clients
         </a>
     </nav>
-    <div>
+    <table style="padding: 15px">
+        <tr>
+            <th>Nom</th>
+            <th>Cost</th>
+            <th>Descripció</th>
+            <th>Quantitat</th>
+            <th></th>
+        </tr>
         @foreach ($products as $product)
-            <div>
-                <div>{{ $product->name }}, {{ $product->price }}, {{ $product->description }}, {{ $product->quantity }}</div>
-                <button onclick="location.href='{{ url('/product/'.$product->id.'/warn') }}'">Delete</button>
+            <tr>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->price }}</td>
+                <td>{{ $product->description }}</td>
+                <td>{{ $product->quantity }}</td>
+                <td><button onclick="location.href='{{ url('/product/'.$product->id.'/warn') }}'" style="border: 1px solid black; border-radius: 5px; background-color: red; color: white; font-weight: bold">Delete</button></td>
                 <!-- Aquest botó ens porta directament al link d'eliminació del producte -->
-            </div>
+            </tr>
         @endforeach
+    </table>
+
+    <br>
+    <div>
+        <label>Click here to insert a new product</label>
         <br>
-        <div>
-            <label>Click here to insert a new product</label>
-            <br>
-            <button onclick="location.href='{{ url('/product/insert') }}'">Insert</button>
-        </div>
+        <button onclick="location.href='{{ url('/product/insert') }}'" style="background-color: green; color: white; border-radius: 5px">Insert</button>
     </div>
 </x-app-layout>
+
 
