e2ab4f7ce66fa568abdc7138fa7d68c59c23f558
Keita58 <marc-s-26@hotmail.com>
2024-09-17 10:29:24 +0200

    Proves de l'exercici 3

--- 2n/Acces a dades/Exercicis/Diccionaris/src/Ex3.java
+++ 2n/Acces a dades/Exercicis/Diccionaris/src/Ex3.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class Ex3 {
 
@@ -9,7 +11,8 @@ public class Ex3 {
 		int casos = ohowo.nextInt();
 		
 		for(int i = 0; i < casos; i++) {
-			Map<String, String> valoracio = new HashMap<>();
+			//Map<String, String> valoracio = new HashMap<>();
+			SortedMap<String, String> valoracio = new TreeMap<>();
 			int linies = ohowo.nextInt();
 			ohowo.nextLine();
 			float mitjana = 0;
@@ -25,9 +28,11 @@ public class Ex3 {
 			
 			int min = 11;
 			for(String clau : valoracio.keySet()) {
-				if(Integer.parseInt(valoracio.get(clau)) < min)
+				if(Integer.parseInt(valoracio.get(clau)) < min) {
 					min = Integer.parseInt(valoracio.get(clau));
+				}
 			}
+			
 			for(Map.Entry<String, String> res : valoracio.entrySet()) {
 				if(Integer.parseInt(res.getValue()) == min) {
 					System.out.print(" " + res.getKey());
@@ -37,9 +42,11 @@ public class Ex3 {
 			
 			int max = 0;
 			for(String clau : valoracio.keySet()) {
-				if(Integer.parseInt(valoracio.get(clau)) > max)
+				if(Integer.parseInt(valoracio.get(clau)) > max) {
 					max = Integer.parseInt(valoracio.get(clau));
+				}
 			}
+
 			for(Map.Entry<String, String> res : valoracio.entrySet()) {
 				if(Integer.parseInt(res.getValue()) == max) {
 					System.out.print(" " + res.getKey());
