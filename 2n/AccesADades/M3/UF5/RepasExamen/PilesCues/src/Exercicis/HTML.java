38036bb6bb09bca2e4c26cc9cef6aa5de8337588
Keita58 <marc-s-26@hotmail.com>
2024-10-07 23:10:29 +0200

    Començ repàs examen M3 UF5

---
+++ 2n/AccesADades/M3/UF5/RepasExamen/PilesCues/src/Exercicis/HTML.java
@@ -0,0 +1,21 @@
+package Exercicis;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class HTML {
+
+    public static void main(String[] args) {
+        Scanner ohowo = new Scanner(System.in);
+        Deque<String> cua = new LinkedList<>();
+        int linies = ohowo.nextInt();
+        ohowo.nextLine();
+
+        for(int i = 0; i < linies; i++) {
+            
+        }
+
+        ohowo.close();
+    }
+}
