0ec7edda97cec18e0d0746ec83035f9832c31d1d
Keita58 <marc-s-26@hotmail.com>
2024-12-23 22:44:01 +0100

    Exercicis C#

---
+++ 2n/AccesADades/M6/UF2/Exercicis/C#/Activitats Eloi/Codi.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activitats
+{
+    internal class Codi
+    {
+        static void Main(string[] args)
+        {
+            //Activitat1();
+            //Activitat2();
+            //Activitat3();
+            //Activitat4();
+            //Activitat5();
+            //Activitat6();
+            //Activitat7();
+            //Activitat8();
+            //Activitat9();
+            Activitat10();
+        }
+
+        static void Activitat1()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 10 == 0 || (num >= 100 && num <= 200))
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+        }
+
+        static void Activitat2()
+        {
+            char[] cadena = Console.ReadLine().ToCharArray();
+            char aux = cadena[0];
+            cadena[0] = cadena[cadena.Length - 1];
+            cadena[cadena.Length - 1] = aux;
+
+            for(int i = 0; i < cadena.Length; i++)
+            {
+                Console.Write(cadena[i]);
+            }
+        }
+
+        static void Activitat3()
+        {
+            int files = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < files; i++)
+            {
+                for (int j = 0; j < (i + 1); j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+
+        static void Activitat4()
+        {
+            int files = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i < files + 1; i++)
+            {
+                for (int k = files; k > i; k--)
+                {   
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Activitat5()
+        {
+            int files = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < files + 1; i++)
+            {
+                for (int k = files; k > i; k--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < (i + (i-1)); j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = files - 1; i > 0; i--)
+            {
+                for (int k = files; k > i; k--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < (i + (i - 1)); j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Activitat6()
+        {
+            int[] num = new int[10];
+
+            for (int i = 0; i < num.Length; i++)
+                num[i] = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int nRandom = rnd.Next(1, 11);
+                num[nRandom - 1]++;
+                Console.WriteLine(nRandom);
+            }
+            for (int i = 0; i < num.Length; i++)
+            {
+                if(num[i] > 0)
+                Console.WriteLine(i+1 + ": " + num[i] + " vegades");
+            }
+        }
+
+        static void Activitat7()
+        {
+            Random rnd = new Random();
+            List<int> numsGenerats = new List<int>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                int num = rnd.Next(1, 101);
+                numsGenerats.Add(num);
+            }
+
+            List<int> res = numsGenerats.OrderByDescending(x => x).ToList();
+
+            Console.WriteLine("Sense ordenar:");
+
+            for (int i = 0; i < numsGenerats.Count; i++)
+                Console.WriteLine(numsGenerats[i]);
+
+            Console.WriteLine();
+            Console.WriteLine("Ordenats: ");
+
+            for (int i = 0; i < res.Count; i++)
+                Console.WriteLine(res[i]);
+        }
+
+        static void Activitat8() 
+        {
+            char[] cadenaText = Console.ReadLine().ToLower().ToCharArray();
+            int[] nums = new int[2]; // 0 -> vocals | 1 -> consonants
+
+            for (int i = 0; i < cadenaText.Length; i++)
+            {
+                if (cadenaText[i] == 'a' || cadenaText[i] == 'e' || cadenaText[i] == 'i' || cadenaText[i] == 'o' || cadenaText[i] == 'u')
+                    nums[0]++;
+                else
+                    nums[1]++;
+            }
+
+            Console.WriteLine("Vocals: " + nums[0]);
+            Console.WriteLine("Consonants: " + nums[1]);
+        }
+
+        static void Activitat9()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Recursiu(0, 1, num));
+
+            //Funció dins de funció :0
+            int Recursiu(int numAnt, int numAct, int vegades)
+            {
+                if (vegades > 1)
+                {
+                    numAnt = numAnt + numAct;
+                    vegades--;
+                    return Recursiu(numAct, numAnt, vegades);
+                }
+                else if (vegades == 0)
+                    return 0;
+                return
+                    numAct;
+            }
+        }
+
+        static void Activitat10()
+        {
+            Random rnd = new Random();
+            List<KeyValuePair<int, int>> num = new List<KeyValuePair<int, int>>();
+
+
+        }
+    }
+}
---
+++ 2n/AccesADades/M6/UF2/Exercicis/C#/Activitats jo-el/Activitats.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activitat_1
+{
+    internal class Activitats
+    {
+        static void Main(string[] args)
+        {
+            //Patata();
+            //Capitals();
+            //Dreta();
+            Bolas();
+        }
+
+        static void Patata()
+        {
+            int casos = int.Parse(Console.ReadLine());
+            while (casos > 0)
+            {
+                int vegades = int.Parse(Console.ReadLine());
+                while (vegades > 0)
+                {
+                    Console.WriteLine("No ofendre al president patata");
+                    vegades--;
+                }
+                casos--;
+            }
+        }
+
+        static void Capitals()
+        {
+            int casos = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < casos; i++)
+            {
+                Dictionary<String, String> ciutats = new Dictionary<String, String>();
+                int aux = int.Parse(Console.ReadLine());
+
+                for(int j = 0; j < aux - 1; j++)
+                {
+                    String[] capitals = Console.ReadLine().Split('-');
+                    ciutats.Add(capitals[0], capitals[1]);
+                }
+
+                string ciutat = Console.ReadLine();
+                if (ciutats.ContainsKey(ciutat))
+                {
+                    Console.WriteLine(ciutats[ciutat]);
+                }
+                else
+                {
+                    Console.WriteLine("NO HO SE");
+                }
+            }
+        }
+
+        static void Dreta()
+        {
+            int midaArray = int.Parse(Console.ReadLine());
+
+            List<int> numeros = new List<int>();
+
+            string[] aux = Console.ReadLine().Split(' ');
+
+            for(int i = 0; i < midaArray; i++)
+            {
+                numeros.Add(int.Parse(aux[i]));
+            }
+
+            int ultim = numeros[midaArray - 1];
+            numeros.RemoveAt(midaArray - 1);
+            numeros.Insert(0, ultim);
+
+            for (int i = 0; i < numeros.Count; i++)
+            {
+                Console.Write(numeros[i]);
+                Console.Write(' ');
+            }  
+        }
+
+        static void Bolas()
+        {
+            int casos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < casos; i++)
+            {
+                string paraula = Console.ReadLine();
+                char[] pSeparada = new char[paraula.Length];
+                for (int j = 0; j < paraula.Length; j++) { pSeparada[j] = paraula[j]; }
+                string[] posicions = Console.ReadLine().Split(' ');
+
+                char aux = pSeparada[int.Parse(posicions[0])];
+                pSeparada[int.Parse(posicions[0])] = pSeparada[int.Parse(posicions[1])];
+                pSeparada[int.Parse(posicions[1])] = aux;
+
+                for (int j = 0; j < pSeparada.Length; j++)
+                {
+                    Console.Write(pSeparada[j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
