6a4b183e6b25d419f6ee21e4fd32e618777fde3e
Keita58 <marc-s-26@hotmail.com>
2025-02-20 23:35:07 +0100

    Entregada ruleta

--- 2n/ProgramacioMobil/UF3/act1/lib/afegir.dart
+++ 2n/ProgramacioMobil/UF3/act1/lib/afegir.dart
@@ -1,7 +1,28 @@
 
 import 'package:flutter/material.dart';
 
-class Afegir extends StatelessElement {
-  Afegir(super.widget);
+class Afegir extends StatelessWidget {
+  const Afegir({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 5.0,
+        shadowColor: Colors.black,
+        centerTitle: true,
+        foregroundColor: Colors.white,
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text('Afegir activitat'),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
 }
